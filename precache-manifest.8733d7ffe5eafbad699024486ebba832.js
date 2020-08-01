8c4e5abcbb6707bd999f1c31daae7a8ce717be8c
Roshan-Horo <35597955+Roshan-Horo@users.noreply.github.com>
2020-08-01 10:52:19 +0530

    Updates

---
+++ precache-manifest.8733d7ffe5eafbad699024486ebba832.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12ef6b5353cb1179848dab28215e897c",
+    "url": "/firegram/index.html"
+  },
+  {
+    "revision": "62fdf8e0818b46361b8d",
+    "url": "/firegram/static/css/main.0aa7fd0c.chunk.css"
+  },
+  {
+    "revision": "47009bb72e3a16741577",
+    "url": "/firegram/static/js/2.28e6e280.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/firegram/static/js/2.28e6e280.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62fdf8e0818b46361b8d",
+    "url": "/firegram/static/js/main.502059bb.chunk.js"
+  },
+  {
+    "revision": "a20b20e63cdcd0ca9e81",
+    "url": "/firegram/static/js/runtime-main.9968160c.js"
+  }
+]);
